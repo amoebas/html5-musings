6b7047e078baf863c299e60407f3912f731c6695
Stig Lindqvist <stig@stojg.net>
2011-07-19 22:40:14 +0200

    Removed default favicons, added robots txt
    Changed the text from lorem to an excuse
    Block all robots from indexing
    
    Removed the favicons that comes from the boilerplate

--- index.php
+++ index.php
@@ -8,14 +8,13 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
-	<title></title>
+	<title>Musings about front-end code | stojg.se</title>
 	<meta name="description" content="This is a small test">
 	<meta name="author" content="stig lindqvist">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
 	<link rel="shortcut icon" href="/favicon.ico">
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
 	<link rel="stylesheet" href="css/style.css?v=2">
 
 	<!--[if lt IE 9]>
@@ -25,32 +24,27 @@
 <body>
 	<div id="header-container">
 		<header class="wrapper">
-			<h1 id="title">Your title</h1>
-			<nav>
-				<ul>
+			<h1 id="title">musings</h1>
+			<!--<nav>
+				<!--<ul>
 					<li><a href="#">Some</a></li>
 					<li><a href="#">navigation</a></li>
 					<li><a href="#">links</a></li>
 				</ul>
-			</nav>
+			</nav>-->
 		</header>
 	</div>
 	<div id="main" class="wrapper">
-		<aside>
-			<h3>Something aside</h3>
-		</aside>
+		<!--<aside>
+			<h3>Something is amiss</h3>
+		</aside>-->
 		<article>
 			<header>
-				<h2>Your article heading</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra. Pellentesque auctor neque quis nisl lacinia id rutrum lacus venenatis.</p>
+				<h2>Playground and demos</h2>
+				<p>This is not a real site in case you thought that. This a playground and demo site. The content you're searching might be found at <a href="http://stojg.se/">http://stojg.se/</a></p>
 			</header>
-			<h3>A smaller heading</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra. Pellentesque auctor neque quis nisl lacinia id rutrum lacus venenatis.</p>	
-			<h3>A smaller heading</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra. Pellentesque auctor neque quis nisl lacinia id rutrum lacus venenatis.</p>
-			<footer>
-			<h3>About the author</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+			<h3>Sources</h3>
+				<p>All the code in this subsection can be found at <a href="https://github.com/stojg/musings">https://github.com/stojg/musings</p>
 			</footer>	
 		</article>
 	</div>
